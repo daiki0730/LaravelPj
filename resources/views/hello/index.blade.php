af0a7fab4cfe938cc426b5215a5f9c90965d39a6
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-09 19:14:48 +0900

    messageコンポーネントを組み込む
    @slotディレクティブを利用して変数に値を渡す

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,6 +9,16 @@
 
 @section('content')
   <p>本文のコンテンツ</p>
+
+  @component('components.message')
+    @slot('msg_title')
+    CAUTION
+    @endslot
+
+    @slot('msg_content')
+    これはメッセージの表示です。
+    @endslot
+  @endcomponent
 @endsection
 
 @section('footer')
