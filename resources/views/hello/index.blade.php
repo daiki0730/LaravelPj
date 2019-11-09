ec5b30933ba442d7e6edd88d4b126a1c4b8a3889
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-09 18:24:03 +0900

    継承レイアウトの作成
    WEBページに使うテンプレートを用意

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,17 +1,16 @@
-<html>
-<head>
-  <title>Hello</title>
-  <style>
-  body {font-size:16pt; color:#999;}
-  h1 {font-size:50pt; text-align:right; color:#f6f6f6; margin:-20px 0px -30px 0px; letter-spacing:-4pt;}
-  </style>
-</head>
-<body>
-  <h1>Blade/INDEX</h1>
-  <ol>
-    @foreach($data as $item)
-    <li>{{$item}}
-    @endforeach
-  </ol>
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+  <p>本文のコンテンツ</p>
+@endsection
+
+@section('footer')
+copyright 2017 tuyano.
+@endsection
