31d57be0c7bc9a6fb94034be78958e5bfa32ed9b
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-09 19:59:16 +0900

    変数の値を設置

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,7 +9,13 @@ class HelloController extends Controller
 {
     public function index()
     {
-      $data = ['one','two','three','four','five'];
+      $data = [
+        ['name'=>'hyde', 'mail'=>'l@yamada'],
+        ['name'=>'tetsu', 'mail'=>'a@yamada'],
+        ['name'=>'ken', 'mail'=>'r@yamada'],
+        ['name'=>'yukihiro', 'mail'=>'c@yamada']
+      ];
+
       return view('hello.index',['data'=>$data]);
     }
 }
