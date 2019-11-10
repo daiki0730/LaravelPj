2cfecd122321b746aed76e3532cc35a2821037ab
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-10 16:10:06 +0900

    HelloMiddlewareを修正
    mergeメソッドを使用

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -8,6 +8,13 @@ class HelloMiddleware
 {
     public function handle($request, Closure $next)
     {
+        $data = [
+          ['name'=>'hyde', 'mail'=>'l@yamada'],
+          ['name'=>'tetsu', 'mail'=>'a@yamada'],
+          ['name'=>'ken', 'mail'=>'r@yamada'],
+          ['name'=>'yukihiro', 'mail'=>'c@yamada']
+        ];
+        $request->merge(['data'=>$data]);
         return $next($request);
     }
 }
