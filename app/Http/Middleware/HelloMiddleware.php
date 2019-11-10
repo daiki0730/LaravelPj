8465aa850a976152cf3615b19d74f9d39e9bf2bb
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-10 16:03:31 +0900

    不要な記述を削除

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -6,13 +6,6 @@ use Closure;
 
 class HelloMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle($request, Closure $next)
     {
         return $next($request);
