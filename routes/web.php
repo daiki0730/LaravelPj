7746675cd01a181cab189a0b4447d139f414849e
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-10 20:05:27 +0900

    middlewareメソッドをルートで利用
    ミドルウェアグループ名を引数に指定

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello','HelloController@index');
+Route::get('hello','HelloController@index')
+  ->middleware('helo');
