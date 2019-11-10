5493264e1214987d3cba7e130b7213238ec55401
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-10 14:22:27 +0900

    不要な記述を削除
    登録機能を使う予定が無いため

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -6,21 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class HelloServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
     public function boot()
     {
         //
