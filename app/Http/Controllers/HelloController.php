49d709c6ac4f5c69247f1e346b47fb71f0fdb1fc
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-10 15:19:22 +0900

    コントローラを修正

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,13 +9,6 @@ class HelloController extends Controller
 {
     public function index()
     {
-      $data = [
-        ['name'=>'hyde', 'mail'=>'l@yamada'],
-        ['name'=>'tetsu', 'mail'=>'a@yamada'],
-        ['name'=>'ken', 'mail'=>'r@yamada'],
-        ['name'=>'yukihiro', 'mail'=>'c@yamada']
-      ];
-
-      return view('hello.index',['data'=>$data]);
+      return view('hello.index',['message'=>'hello!']);
     }
 }
