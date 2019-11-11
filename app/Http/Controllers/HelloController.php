764ca89cce70f557be092d425d6aad147cc81897
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-11 19:36:43 +0900

    クエリー文字にバリデータを適用する
    queryメソッドでクエリー文字列を配列にまとめて返す。

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,12 +11,21 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-      return view('hello.index',['msg' => 'フォームを入力:']);
+      $validator = Validator::make($request->query(), [
+        'id' => 'required',
+        'pass' => 'required',
+      ]);
+      if ($validator->fails()) {
+        $msg ='クエリーに問題があります';
+      } else {
+        $msg ='ID/PASSを受け付けました。フォームを入力ください。';
+      }
+      return view('hello.index', ['msg'=>$msg, ]);
     }
 
     public function post(Request $request)
     {
-      $validator = Validator::make($request->all()), [
+      $validator = Validator::make($request->all(), [
         'name' => 'required',
         'mail' => 'email',
         'age' => 'numeric|between:0,150',
