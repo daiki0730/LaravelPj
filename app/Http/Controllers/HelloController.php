4db1899edad109d0b466b2d1bb986e3af8962a3a
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-14 20:31:54 +0900

    条件分岐を削除

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,12 +11,6 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-      if ($request->hasCookie('msg'))
-      {
-        $msg = 'Cookie: ' . $request->cookie('msg');
-      } else {
-        $msg = '※クッキーはありません';
-      }
 
       return view('hello.index', ['msg'=>$msg]);
     }
