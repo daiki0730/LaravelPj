33f318015583e2944db6a7a05bcc4c69e62bfe0e
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-18 19:57:48 +0900

    スペルミスを修正

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -74,14 +74,14 @@ class HelloController extends Controller
     public function del(Request $request)
     {
         $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param)
+        $item = DB::select('select * from people where id = :id', $param);
         return view('hello.del', ['form' => $item[0]]);
     }
 
     public function remove(Request $request)
     {
         $param = ['id' => $request->id];
-        $item = DB::delete('delete * from people where id = :id', $param);
+        DB::delete('delete from people where id = :id', $param);
         return redirect('/hello');
     }
 
