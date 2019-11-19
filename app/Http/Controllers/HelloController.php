507ebaa119955bd0bd464133b54634d4f4b4b091
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-19 15:45:56 +0900

    演算記号を使用した検索を記述
    whereの引数を3つに増やす

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -81,9 +81,10 @@ class HelloController extends Controller
 
     public function show(Request $request)
     {
-        $id = $request->id;
-        $item = DB::table('people')->where('id',$id)->first();
-        return view('hello.show', ['item' => $item]);
+        $id  = $request->id;
+        $items = DB::table('people')->where('id','<=',$id)->get();
+        return view('hello.show', ['items' => $items]);
     }
+
 
 }
