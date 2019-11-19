caa97c8815f79f47f413cef10b1ac3531849d806
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-19 17:19:03 +0900

    removeアクションにクエリビルダを使用
    deleteメソッドを利用し記述量を削減

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -67,15 +67,15 @@ class HelloController extends Controller
 
     public function del(Request $request)
     {
-        $item = DB::table('people');
+        $item = DB::table('people')
             ->where('id',$request->id)->first();
         return view('hello.del', ['form' => $item]);
     }
 
     public function remove(Request $request)
     {
-        $param = ['id' => $request->id];
-        DB::delete('delete from people where id = :id', $param);
+        DB::table('people')
+            ->where('id',$request->id)->delete();
         return redirect('/hello');
     }
 
