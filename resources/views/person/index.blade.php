7ef69e8641b2c4a52606fd6dc1e5614aeea789f0
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-20 19:44:03 +0900

    viewを修正
    getDataの値を出力

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -9,12 +9,10 @@
 
 @section('content')
     <table>
-    <tr><th>Name</th><th>MAil</th><th>Age</th></tr>
+    <tr><th>Data</th></tr>
     @foreach ($items as $item)
         <tr>
-            <td>{{$item->name}}</td>
-            <td>{{$item->mail}}</td>
-            <td>{{$item->age}}</td>
+            <td>{{$item->getData()}}</td>
         </tr>
     @endforeach
     </table>
