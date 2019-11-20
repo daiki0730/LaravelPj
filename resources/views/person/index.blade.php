9adc0415af66d28e39dc3b271ff3276841d52443
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-20 19:17:42 +0900

    viewに詳細を記述
    $itemsで受け取ったレコードデータを出力

---
+++ resources/views/person/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Person.index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <table>
+    <tr><th>Name</th><th>MAil</th><th>Age</th></tr>
+    @foreach ($items as $item)
+        <tr>
+            <td>{{$item->name}}</td>
+            <td>{{$item->mail}}</td>
+            <td>{{$item->age}}</td>
+        </tr>
+    @endforeach
+    </table>
+@endsection
+
+@section('footer')
+copyright 2019 daiki.
+@endsection
