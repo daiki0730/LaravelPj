30ad114faea15861e2c5210ad8794b751a61a4b9
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-23 19:37:30 +0900

    不要な記述を削除

--- database/migrations/2019_11_23_101704_create_boards_table.php
+++ database/migrations/2019_11_23_101704_create_boards_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateBoardsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('boards', function (Blueprint $table) {
@@ -19,11 +14,6 @@ class CreateBoardsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('boards');
