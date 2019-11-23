f51a8482d6d323a868150f7bcf3a3d9279bbf74d
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-23 16:02:05 +0900

    モデルを修正する
    新しいレコードを保存する処理として作成

--- app/Person.php
+++ app/Person.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
+  protected $guarded = array('id');
 
+  public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
+
+    public function getData()
+    {
+        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+    }
 }
