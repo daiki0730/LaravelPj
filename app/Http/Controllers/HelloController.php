7220aa76a2eeb10afd859a65c8c724d65a22549f
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-24 21:08:08 +0900

    ses_getアクションを追加
    hello/sessionにアクセスしたときの処理。
    値を取得して$変数sesdataに格納

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -94,6 +94,11 @@ class HelloController extends Controller
         return view('hello.rest');
     }
 
+    public function ses_get(Request $request)
+    {
+        $sesdata = $request->session()->get('msg');
+        return view('hello.session',['session_data' => $sesdata]);
+    }
 }
 
 
