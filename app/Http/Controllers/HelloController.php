b91b70938d47d46dcf0be7a5a5748dba02fe8cd8
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-25 19:59:39 +0900

    Auth::userメソッドを利用
    モデルクラスUserから利用

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,15 +8,17 @@ use App\Http\Requests\HelloRequest;
 use Validator;
 use Illuminate\Support\Facades\DB;
 use App\Person;
+use Illuminate\Support\Facades\Auth;
 
 class HelloController extends Controller
 {
 
     public function index(Request $request)
     {
+      $user = Auth::user();
       $sort = $request->input('name','mail','age');
       $items = Person::orderBy($sort, 'asc')->paginate(5);
-      $param = ['items' => $items, 'sort' => $sort];
+      $param = ['items' => $items, 'sort' => $sort, 'user' => $user];
       return view('hello.index', $param);
     }
 
