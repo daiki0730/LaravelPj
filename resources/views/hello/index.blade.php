eb647fe46bb6d950dc575f8747345ece707a743b
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-25 17:47:03 +0900

    スタイルを変更

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.helloapp')
 <style>
-  .pagination { font-size:10pt; }
-  .pagination li { display:inline-block; }
+    .pagination { font-size:12pt; }
+    .pagination li { display:inline-block }
+    tr th a:link { color: white; }
+    tr th a:visited { color: white; }
+    tr th a:hover { color: white; }
+    tr th a:active { color: white; }
 </style>
 
 @section('title', 'Index')
