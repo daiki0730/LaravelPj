8c5a629d1396152155ebdca95d89171dc12a5116
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-25 18:39:03 +0900

    cssを読み込み

--- resources/views/layouts/helloapp.blade.php
+++ resources/views/layouts/helloapp.blade.php
@@ -15,8 +15,8 @@
         border-bottom:solid 1px #ccc; color:#ccc; }
     th {background-color:#999; color:fff; padding:5px 10px; }
     td {border: solid 1px #aaa; color:#999; padding:5px 10px; }
-
     </style>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
     <h1>@yield('title')</h1>
